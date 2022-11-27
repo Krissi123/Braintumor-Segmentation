debba11fe33b0dcbc678ee74026292b8d4f2ea1f
Neil Bruce <njbruce@gmail.com>
2022-11-27 15:02:48 +0000

    mobilenet run script

--- model_src/classification_mobilenetv2_4to3channelconv_gcloud.py
+++ model_src/classification_mobilenetv2_4to3channelconv_gcloud.py
@@ -9,25 +9,24 @@ from tensorflow.keras.models import Sequential
 
 RSEED = 123
 
-def train_models(batch_size=32):
-    run_description = """
-    Fully trained models 
-     - classify each slice by tumour/tissue regions in the segmentation
-    """
-    dataset = 'full_data_stratified'
+def train_models(
+    batch_size=32, 
+    patience=0, 
+    min_delta=0.001,
+    dropout_rate=0.2,
+    initial_learning_rate=0.0001
+):
     
-    # Set information for mlflow
-    mlflow_tracking_uri = os.getenv('MLFLOW_URI')
-    if mlflow_tracking_uri:
-        mlflow.set_tracking_uri(mlflow_tracking_uri)
-    mlflow_expt = os.getenv('CLASSIFICATION_EXPT')
-    if mlflow_expt:
-        mlflow.set_experiment(mlflow_expt)
-    mlflow.tensorflow.autolog(log_models=False)
-    mlflow.set_tags({
-            'dataset': dataset,
-            'mlflow.note.content': run_description,
-    })
+    run_name_params = (
+        f'bs{batch_size}'
+        f'_pat{patience}'
+        f'_del{min_delta}'
+        f'_dr{dropout_rate}'
+        f'_lr{initial_learning_rate}'
+    )
+    
+    print("Using parameters")
+    print(run_name_params)
 
     # Set up gcloud TPUs
     cluster_resolver = tf.distribute.cluster_resolver.TPUClusterResolver(tpu='local')
@@ -57,6 +56,10 @@ def train_models(batch_size=32):
         image_size=(img_height, img_width),
         batch_size=batch_size)
 
+    mlflow.log_param('ds_batch_size', batch_size)
+    mlflow.log_param('ds_validation_batch_size', batch_size)
+    
+
     class_names = train_ds.class_names
 
     # Calculate class weights ofr weighting accuracy
@@ -85,157 +88,131 @@ def train_models(batch_size=32):
     scaled_height = img_height - 2*margin
     scaled_width = img_width - 2*margin
 
-    # Build layers for model
-    crop_layer = tf.keras.layers.Cropping2D(margin)
-    rescale_initial = tf.keras.layers.Rescaling(1./255)
-    conv_4to3_channel = tf.keras.layers.Conv2D(3,1,padding='same', activation='tanh')
-    trained_base_model = tf.keras.applications.MobileNetV2(
-        input_shape=(scaled_width,scaled_height),
-        include_top=False
-        weights='imagenet'
-    )
-    trained_base_model.trainable = False
+    # Build layers for model with fixed base
+    with strategy.scope():
+        crop_layer = tf.keras.layers.Cropping2D(margin)
+        rescale_initial = tf.keras.layers.Rescaling(1./255)
+        conv_4to3_channel = tf.keras.layers.Conv2D(3,1,padding='same',activation='tanh')
+        base_model = tf.keras.applications.MobileNetV2(
+            input_shape=(scaled_width,scaled_height,3),
+            include_top=False,
+            weights='imagenet'
+        )
+        global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+        prediction_layer = tf.keras.layers.Dense(num_classes)
+
+        base_model.trainable = False
+        
+        inputs = tf.keras.Input(shape=(img_width, img_height, 4))
+        x = crop_layer(inputs)
+        x = rescale_initial(x)
+        x = conv_4to3_channel(x)
+        x = base_model(x, training=False)
+        x = global_average_layer(x)
+        x = tf.keras.layers.Dropout(dropout_rate)(x)
+        outputs = prediction_layer(x)
     
-
-
-    with mlflow.start_run(nested=True):
-        # Run small model
-        with strategy.scope():
-            small_model = Sequential([
-            layers.Rescaling(1./(2**8-1), input_shape=(img_height, img_width, 4)),
-            layers.Conv2D(16, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(32, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Flatten(),
-            layers.Dense(128, activation='relu'),
-            layers.Dense(num_classes)
-            ])
-            small_model.compile(optimizer='adam',
-                        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                        metrics=['accuracy'])
-
-        small_model.summary()
-        mlflow.set_tags({
-            'dataset': dataset,
-            'mlflow.note.content': small_model.summary(),
-            })
-        mlflow.log_param('ds_batch_size', batch_size)
-        mlflow.log_param('ds_validation_batch_size', batch_size)
-        small_model_epochs=50
-        small_model_history = small_model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=small_model_epochs,
-        class_weight=class_weight
+        earlystopping = tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience,
+            min_delta=min_delta,
+            )
+        
+        model = tf.keras.Model(inputs, outputs)
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learning_rate,),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'],
         )
 
-    with mlflow.start_run(nested=True):
-        # Create and fit larger model
-        with strategy.scope():
-            larger_model = Sequential([
-            layers.Rescaling(1./(2**8-1), input_shape=(img_height,
-                                        img_width,
-                                        4)),
-            layers.Conv2D(8, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(16, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(32, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Flatten(),
-            layers.Dropout(0.2),
-            layers.Dense(128, activation='relu'),
-            layers.Dropout(0.2),
-            layers.Dense(128, activation='relu'),
-            layers.Dropout(0.2),
-            layers.Dense(num_classes, name="outputs")
-            ])
-
-            larger_model.compile(optimizer='adam',
-                        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                        metrics=['accuracy'])
-
-        larger_model.summary()
-        mlflow.set_tags({
-            'dataset': dataset,
-            'mlflow.note.content': larger_model.summary(),
-            })
-        mlflow.log_param('ds_batch_size', batch_size)
-        mlflow.log_param('ds_validation_batch_size', batch_size)
-        larger_model_epochs = 100
-        larger_model_history = larger_model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=larger_model_epochs,
-        class_weight=class_weight
+        
+    # Initial fit of classification and 4 to 3 channel layers
+    with mlflow.start_run(
+        run_name=f'fixed_{run_name_params}',
+        nested=True
+    ):
+        fixed_base_epochs=80
+        history_fixed_base = model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=fixed_base_epochs,
+            class_weight=class_weight,
+            callbacks=[earlystopping],
         )
 
-    with mlflow.start_run(nested=True):
-        # Data augmentation layer
-        data_augmentation = keras.Sequential(
-        [
-            layers.RandomFlip("horizontal",
-                            input_shape=(img_height,
-                                        img_width,
-                                        4)),
-            layers.RandomRotation(0.1),
-            layers.RandomZoom(0.1),
-        ]
-        )
-        # Apply augmentation to training data set
-        aug_ds = train_ds.map(lambda x, y: (data_augmentation(x, training=True), y), num_parallel_calls=AUTOTUNE)
-        aug_ds = aug_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-
-        # Create and fit larger model with augmented data
-        with strategy.scope():
-            augmented_model = Sequential([
-            layers.Rescaling(1./(2**8-1), input_shape=(img_height,
-                                        img_width,
-                                        4)),
-            layers.Conv2D(8, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(16, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(32, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(64, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Conv2D(128, 3, padding='same', activation='relu'),
-            layers.MaxPooling2D(),
-            layers.Flatten(),
-            layers.Dropout(0.2),
-            layers.Dense(128, activation='relu'),
-            layers.Dropout(0.2),
-            layers.Dense(128, activation='relu'),
-            layers.Dropout(0.2),
-            layers.Dense(num_classes, name="outputs")
-            ])
-
-            augmented_model.compile(optimizer='adam',
-                        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                        metrics=['accuracy'])
-
-        mlflow.set_tags({
-            'dataset': dataset + '(augmented)',
-            'mlflow.note.content': augmented_model.summary(),
-            })
-        mlflow.log_param('ds_batch_size', batch_size)
-        mlflow.log_param('ds_validation_batch_size', batch_size)
-        augmented_model_epochs = 200
-        augmented_model_history = augmented_model.fit(
-        aug_ds,
-        validation_data=val_ds,
-        epochs=augmented_model_epochs,
-        class_weight=class_weight
+    # Relax top layers of base model
+    base_model.trainable = True
+    fix_below_layer = 100
+    for layer in base_model.layers[:fix_below_layer]:
+        layer.trainable = False
+    with strategy.scope():
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learning_rate/10.0),
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'],
         )
 
+    with mlflow.start_run(
+        run_name=f'partial_{run_name_params}',
+        nested=True
+    ):
+        partial_relax_epochs=history_fixed_base.epoch[-1] + 100 
+        history_partial_relax = model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=partial_relax_epochs,
+            initial_epoch=history_fixed_base.epoch[-1]+1,
+            class_weight=class_weight,
+            callbacks=[earlystopping],
+        )
+
+    # Fully relax model
+    model.trainable = True
+
+    with strategy.scope():
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=initial_learning_rate/10.0),
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy'],
+        )
+
+    with mlflow.start_run(
+        run_name=f'relax_{run_name_params}',
+        nested=True
+    ):
+        full_relax_epochs=history_partial_relax.epoch[-1] + 100
+        history_full_relax = model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=full_relax_epochs,
+            initial_epoch=history_partial_relax.epoch[-1]+1,
+            class_weight=class_weight,
+            callbacks=[earlystopping],
+        )
+  
 
 if __name__ == '__main__':
-    train_models()
+
+    # Set information for mlflow
+    run_description = """
+    Fully trained models 
+     - classify each slice by tumour/tissue regions in the segmentation
+    """
+    dataset = 'full_data_stratified'
+    mlflow_tracking_uri = os.getenv('MLFLOW_URI')
+    if mlflow_tracking_uri:
+        mlflow.set_tracking_uri(mlflow_tracking_uri)
+    mlflow_expt = os.getenv('CLASSIFICATION_EXPT')
+    if mlflow_expt:
+        mlflow.set_experiment(mlflow_expt)
+    mlflow.tensorflow.autolog(log_models=False)
+    mlflow.set_tags({
+            'dataset': dataset,
+            'mlflow.note.content': run_description,
+    }) 
+    
+    # Run training
+    for patience in [0, 3, 5]:
+        train_models(patience=patience)
+
+
